1aae95b693d2f82c20dda90e5a4e620d70969233
张贤森 <544187239@qq.com>
2024-12-25 17:54:33 +0800

    133

---
+++ bytebase/202401010000_create_sj_test_table.sql.sql
@@ -0,0 +1,30 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80012 (8.0.12)
+ Source Host           : localhost:3306
+ Source Schema         : snail_job
+
+ Target Server Type    : MySQL
+ Target Server Version : 80012 (8.0.12)
+ File Encoding         : 65001
+
+ Date: 25/12/2024 17:53:23
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sj_test
+-- ----------------------------
+DROP TABLE IF EXISTS `sj_test`;
+CREATE TABLE `sj_test`  (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci COMMENT = '测试表' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;

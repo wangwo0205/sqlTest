30ac43d151636635fb66eab0aa0b904b3e5a753c
张贤森 <544187239@qq.com>
2024-12-27 14:36:48 +0800

    13

---
+++ bytebase/20241227001_create_table_snail_test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `snail_job`.`Untitled`  (
+                                         `id` int NOT NULL AUTO_INCREMENT,
+                                         `name` varchar(255) NULL,
+                                         PRIMARY KEY (`id`)
+) COMMENT = '测试表';
